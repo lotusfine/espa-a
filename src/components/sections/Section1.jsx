f8c9b3c75aa5adba573feae6b7fd31ab516e00f5
lotusfine <lucasyalet94@gmail.com>
2024-09-18 12:43:27 -0300

    se agregó nuevo titulo

--- src/components/sections/Section1.jsx
+++ src/components/sections/Section1.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Section1 = () => {
   return (
-    <div style={{ height: "100vh", width: "100vw" }}>
+    <div style={{ height: "100vh", width: "100vw", position: "relative" }}>
       <video
         src="/videos/sirenas.mp4"
         autoPlay
@@ -11,9 +11,21 @@ const Section1 = () => {
         loop
         style={{ width: "100%", height: "100%", objectFit: "cover" }}
       />
-      <h1 className="text-white absolute inset-0 flex items-center justify-center text-5xl">
-        Yoga Danza Pilates
-      </h1>
+      
+      {/* Aquí reemplazamos el texto con la imagen */}
+      <img
+        src="/images/titulo.png"
+        alt="Título Yoga Danza Pilates"
+        className="absolute inset-0 m-auto"
+        style={{
+          width: "90%",
+          maxWidth: "600px",
+          top: "20%", // Ajusta la posición
+          left: "30%",
+          transform: "translate(-50%, -50%)",
+          opacity: 0.9, // Ajusta la opacidad si es necesario
+        }}
+      />
     </div>
   );
 };
